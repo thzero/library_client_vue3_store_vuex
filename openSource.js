8b4565b55e8b251488fb044e723386beeaa8a677
thzero <thzero@users.noreply.github.com>
2022-12-20 07:43:57 -0600

    updates for opensource

---
+++ openSource.js
@@ -0,0 +1,53 @@
+export default () => {
+    return [
+        {
+            category: 'client',
+            name: '@thzero/library_client',
+            url: 'https://github.com/thzero/library_client',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_client/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: '@thzero/library_client_vue3',
+            url: 'https://github.com/thzero/library_client_vue3',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_client_vue3/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: '@thzero/library_client_vue3_store_pinia',
+            url: 'https://github.com/thzero/library_client_vue3_store_pinia',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_client_vue3_store_pinia/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: '@thzero/library_common',
+            url: 'https://github.com/thzero/library_common',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common/blob/master/license.md'
+        },
+        {
+            category: 'client',
+            name: 'vue',
+            url: 'https://github.com/vuejs/core',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/vuejs/core/blob/main/LICENSE'
+        },
+        {
+            category: 'client',
+            name: 'vuex',
+            url: 'https://github.com/vuejs/vuex',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/vuejs/vuex/blob/main/LICENSE'
+        },
+        {
+            category: 'client',
+            name: 'vuex-persist',
+            url: 'https://github.com/championswimmer/vuex-persist',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/championswimmer/vuex-persist/blob/master/LICENSE.md'
+        }
+    ];
+}
